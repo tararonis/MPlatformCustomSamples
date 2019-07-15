2487bdc6b1035d43208ce2ecce6bb34fcb1b2011
tararonis <tararonis@gmail.com>
2019-07-15 11:16:50 +0200

    Sample to reproduce problem with Preview in 2.0.2.11288 version

---
+++ !MPlatformCustomSamples/MP_PreviewIssue_1256/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MP_PreviewIssue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelPr = new System.Windows.Forms.Panel();
+            this.Start_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelPr
+            // 
+            this.panelPr.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelPr.Location = new System.Drawing.Point(36, 12);
+            this.panelPr.Name = "panelPr";
+            this.panelPr.Size = new System.Drawing.Size(436, 303);
+            this.panelPr.TabIndex = 0;
+            // 
+            // Start_btn
+            // 
+            this.Start_btn.Location = new System.Drawing.Point(36, 338);
+            this.Start_btn.Name = "Start_btn";
+            this.Start_btn.Size = new System.Drawing.Size(436, 23);
+            this.Start_btn.TabIndex = 1;
+            this.Start_btn.Text = "Start";
+            this.Start_btn.UseVisualStyleBackColor = true;
+            this.Start_btn.Click += new System.EventHandler(this.Start_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 462);
+            this.Controls.Add(this.Start_btn);
+            this.Controls.Add(this.panelPr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPr;
+        private System.Windows.Forms.Button Start_btn;
+    }
+}
+
---
+++ !MPlatformCustomSamples/MP_PreviewIssue_1256/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MPLATFORMLib;
+
+namespace MP_PreviewIssue
+{
+    public partial class Form1 : Form
+    {
+        MPlaylistClass m_objMPlaylist;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_objMPlaylist = new MPlaylistClass();
+        }
+        private void Start_btn_Click(object sender, EventArgs e)
+        {
+            string[] list = {
+            @"M:\TEST_VIDEOS\!1080p 60fpsOri and the Will of the Wisps.mp4", @"M:\TEST_VIDEOS\!audi_a7_borussia_vfl.mp4",
+            @"M:\TEST_VIDEOS\!LG Jazz 1080-30p.mp4",
+            @"M:\TEST_VIDEOS\!NoAudio1080p 60fpsOri and the Will of the Wisps.mp4",
+            @"M:\TEST_VIDEOS\!PATAGONIA 1080p.mp4",
+            @"M:\TEST_VIDEOS\Adele.mp4",
+            @"M:\TEST_VIDEOS\Avengers_2_1080p_6Ch_48kHz-HDTN.mp4" };
+            int period = 1000;
+
+            (m_objMPlaylist as IMPreview).PreviewWindowSet("", panelPr.Handle.ToInt32());
+            (m_objMPlaylist as IMPreview).PreviewEnable("", 1, 1);
+
+            m_objMPlaylist.FilePlayStart();
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                m_objMPlaylist.PreviewFullScreen("", 0, 0);
+                m_objMPlaylist.FileNameSet(list[i], "");
+                m_objMPlaylist.FilePlayStart();
+                Thread.Sleep(period*2);
+                m_objMPlaylist.PreviewFullScreen("", 1, 0);
+                Thread.Sleep(period);              
+                
+            }
+            m_objMPlaylist.PreviewFullScreen("", 0, 0);
+        }
+
+        
+    }
+}
