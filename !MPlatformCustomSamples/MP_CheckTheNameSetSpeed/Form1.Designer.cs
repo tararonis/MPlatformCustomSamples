97b2de3c4f42c7d54a815e91119419d8fc385cc9
Roman Zalesinskiy <tararonis@gmail.com>
2019-08-29 12:18:13 +0200

    Sample that compare openning speed of the 3 Main MPlatform objects

---
+++ !MPlatformCustomSamples/MP_CheckTheNameSetSpeed/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace MP_CheckTheNameSetSpeed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFileMFile_btn = new System.Windows.Forms.Button();
+            this.Stat_lsb = new System.Windows.Forms.ListBox();
+            this.panelpr = new System.Windows.Forms.Panel();
+            this.OpenFileMPlaylist_btn = new System.Windows.Forms.Button();
+            this.OpenFileMMixer_btn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.addParams = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OpenFileMFile_btn
+            // 
+            this.OpenFileMFile_btn.Location = new System.Drawing.Point(295, 176);
+            this.OpenFileMFile_btn.Name = "OpenFileMFile_btn";
+            this.OpenFileMFile_btn.Size = new System.Drawing.Size(222, 38);
+            this.OpenFileMFile_btn.TabIndex = 0;
+            this.OpenFileMFile_btn.Text = "OpenFileMFile";
+            this.OpenFileMFile_btn.UseVisualStyleBackColor = true;
+            this.OpenFileMFile_btn.Click += new System.EventHandler(this.OpenFileMFile_btn_Click);
+            // 
+            // Stat_lsb
+            // 
+            this.Stat_lsb.FormattingEnabled = true;
+            this.Stat_lsb.Location = new System.Drawing.Point(12, 12);
+            this.Stat_lsb.Name = "Stat_lsb";
+            this.Stat_lsb.Size = new System.Drawing.Size(261, 290);
+            this.Stat_lsb.TabIndex = 2;
+            // 
+            // panelpr
+            // 
+            this.panelpr.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelpr.Location = new System.Drawing.Point(295, 12);
+            this.panelpr.Name = "panelpr";
+            this.panelpr.Size = new System.Drawing.Size(222, 152);
+            this.panelpr.TabIndex = 3;
+            // 
+            // OpenFileMPlaylist_btn
+            // 
+            this.OpenFileMPlaylist_btn.Location = new System.Drawing.Point(295, 220);
+            this.OpenFileMPlaylist_btn.Name = "OpenFileMPlaylist_btn";
+            this.OpenFileMPlaylist_btn.Size = new System.Drawing.Size(222, 38);
+            this.OpenFileMPlaylist_btn.TabIndex = 4;
+            this.OpenFileMPlaylist_btn.Text = "OpenFileMPlaylist";
+            this.OpenFileMPlaylist_btn.UseVisualStyleBackColor = true;
+            this.OpenFileMPlaylist_btn.Click += new System.EventHandler(this.OpenFileMPlaylist_btn_Click);
+            // 
+            // OpenFileMMixer_btn
+            // 
+            this.OpenFileMMixer_btn.Location = new System.Drawing.Point(295, 264);
+            this.OpenFileMMixer_btn.Name = "OpenFileMMixer_btn";
+            this.OpenFileMMixer_btn.Size = new System.Drawing.Size(222, 38);
+            this.OpenFileMMixer_btn.TabIndex = 5;
+            this.OpenFileMMixer_btn.Text = "OpenFileMMixer";
+            this.OpenFileMMixer_btn.UseVisualStyleBackColor = true;
+            this.OpenFileMMixer_btn.Click += new System.EventHandler(this.OpenFileMMixer_btn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // addParams
+            // 
+            this.addParams.Location = new System.Drawing.Point(78, 322);
+            this.addParams.Name = "addParams";
+            this.addParams.Size = new System.Drawing.Size(439, 20);
+            this.addParams.TabIndex = 6;
+            this.addParams.Text = "loop=true mxf.force_ffmpeg=true";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "addParams";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 370);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addParams);
+            this.Controls.Add(this.OpenFileMMixer_btn);
+            this.Controls.Add(this.OpenFileMPlaylist_btn);
+            this.Controls.Add(this.panelpr);
+            this.Controls.Add(this.Stat_lsb);
+            this.Controls.Add(this.OpenFileMFile_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenFileMFile_btn;
+        private System.Windows.Forms.ListBox Stat_lsb;
+        private System.Windows.Forms.Panel panelpr;
+        private System.Windows.Forms.Button OpenFileMPlaylist_btn;
+        private System.Windows.Forms.Button OpenFileMMixer_btn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox addParams;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ !MPlatformCustomSamples/MP_CheckTheNameSetSpeed/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MPLATFORMLib;
+
+namespace MP_CheckTheNameSetSpeed
+{
+    public partial class Form1 : Form
+    {
+        MPlaylistClass m_objMPlaylist;
+        MFileClass m_objMFile;
+        MMixerClass m_objMixer;
+        string filePath;
+        DateTime startTime;
+        DateTime endTime;
+        TimeSpan duration;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void OpenFileMFile_btn_Click(object sender, EventArgs e)
+        {
+            MarshalAll();
+            m_objMFile = new MFileClass();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileNames.Length != 0)
+            {
+
+                filePath = openFileDialog1.FileNames[0].ToString();
+
+                SendMes("StatMFile");
+                startTime = DateTime.Now;
+                m_objMFile.FileNameSet(filePath, addParams.Text);
+                endTime = DateTime.Now;
+                CalcTimeSpan();
+                SendMes("MFile takes: " + duration.TotalMilliseconds.ToString() + " TotalMilliseconds");
+                SendMes("With this Params : " + addParams.Text);
+
+                ReInitPr("MFile");
+                m_objMFile.FilePlayStart();
+            }
+
+        }
+
+        private void OpenFileMPlaylist_btn_Click(object sender, EventArgs e)
+        {
+            MarshalAll();
+            m_objMPlaylist = new MPlaylistClass();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileNames.Length != 0)
+            {
+                filePath = openFileDialog1.FileNames[0].ToString();
+                int index = -1;
+                startTime = DateTime.Now;
+                m_objMPlaylist.PlaylistAdd(null, filePath, addParams.Text, ref index, out MItem MItem);
+                endTime = DateTime.Now;
+                CalcTimeSpan();
+                SendMes("MPlaylist takes: " + duration.TotalMilliseconds.ToString() + " TotalMilliseconds");
+                SendMes("With this Params : " + addParams.Text);
+                Marshal.ReleaseComObject(MItem);
+
+                ReInitPr("MPlaylist");
+                m_objMPlaylist.FilePlayStart();
+            }
+
+
+        }
+        private void OpenFileMMixer_btn_Click(object sender, EventArgs e)
+        {
+            MarshalAll();
+            m_objMixer = new MMixerClass();
+            m_objMixer.ObjectStart(null);
+            ReInitPr("MMixer");
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileNames.Length != 0)
+            {
+                for (int i = 0; i < openFileDialog1.FileNames.Length; i++)
+                {
+                    MItem pItem;
+
+                    startTime = DateTime.Now;
+                    m_objMixer.StreamsAdd("", null, openFileDialog1.FileNames[i], addParams.Text, out pItem, 1.00);
+                    endTime = DateTime.Now;
+                    CalcTimeSpan();
+                    SendMes("StreamsAdd takes: " + duration.TotalMilliseconds.ToString() + " TotalMilliseconds");
+                    SendMes("With this Params : " + addParams.Text);
+
+                    if (pItem != null)
+                    {
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(pItem);
+                        GC.Collect();
+                    }
+                }
+                MElement root;
+                m_objMixer.ElementsGetByIndex(0, out root);
+
+                MElement pChild;
+
+                startTime = DateTime.Now;
+                (root as IMElements).ElementsAdd("", "video", "stream_id=stream-000 h=0.5 w=0.5 show=1", out pChild, 0);
+                endTime = DateTime.Now;
+                CalcTimeSpan();
+                SendMes("ElementsAdd takes: " + duration.TotalMilliseconds.ToString() + " TotalMilliseconds");
+                SendMes("With this Params : " + addParams.Text);
+
+                Marshal.ReleaseComObject(pChild);
+
+                m_objMixer.FilePlayStart();
+            }
+
+        }
+
+        private void ReInitPr(string objectName)
+        {
+            if (objectName == "MFile")
+            {
+                m_objMFile.PreviewWindowSet("", panelpr.Handle.ToInt32());
+                m_objMFile.PreviewEnable("", 0, 1);
+
+            }
+            else if (objectName == "MPlaylist")
+            {
+                m_objMPlaylist.PreviewWindowSet("", panelpr.Handle.ToInt32());
+                m_objMPlaylist.PreviewEnable("", 0, 1);
+            }
+            else if (objectName == "MMixer")
+            {
+                m_objMixer.PreviewWindowSet("", panelpr.Handle.ToInt32());
+                m_objMixer.PreviewEnable("", 0, 1);
+            }
+        }
+        void MarshalAll()
+        {
+            SendMes("ReleaseAll");
+            try
+            {
+                m_objMPlaylist.FilePlayStop(0);
+                m_objMPlaylist.ObjectClose();
+                Marshal.ReleaseComObject(m_objMPlaylist);
+            }
+            catch { }
+
+            try
+            {
+                m_objMFile.FilePlayStop(0);
+                m_objMFile.ObjectClose();
+                Marshal.ReleaseComObject(m_objMFile);
+            }
+            catch { }
+            try
+            {
+                m_objMixer.FilePlayStop(0);
+                m_objMixer.ObjectClose();
+                Marshal.ReleaseComObject(m_objMixer);
+            }
+            catch { }
+
+            Thread.Sleep(200);
+            GC.Collect();
+            SendMes("----------------------------");
+            SendMes("");
+        }
+        void SendMes(string mes)
+        {
+            Stat_lsb.Items.Add(mes);
+        }
+        void CalcTimeSpan()
+        {
+            duration = endTime - startTime;
+        }
+    }
+}
